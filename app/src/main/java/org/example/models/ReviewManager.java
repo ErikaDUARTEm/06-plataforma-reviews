16450c320b462d63058c5deba31491218623826a
erika.duarte <erika.duarte.mora@gmail.com>
2024-12-26 21:44:52 -0500

    feat: central repository

--- app/src/main/java/org/example/models/ReviewManager.java
+++ app/src/main/java/org/example/models/ReviewManager.java
@@ -3,7 +3,6 @@ package org.example.models;
 import org.example.services.interfaces.IReviewable;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 
---
+++ app/src/main/java/org/example/repositories/CentralRepository.java
@@ -0,0 +1,27 @@
+package org.example.repositories;
+
+import org.example.models.Dish;
+import org.example.models.Restaurant;
+import org.example.models.Review;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CentralRepository {
+  private static CentralRepository instance;
+  private List<Restaurant> restaurants;
+  private List<Dish> dishes;
+  private List<Review> reviews;
+
+  private CentralRepository(){
+   this.restaurants = new ArrayList<>();
+   this.dishes = new ArrayList<>();
+   this.reviews = new ArrayList<>();
+  }
+  public static synchronized CentralRepository getInstance(){
+    if(instance == null){
+      instance = new CentralRepository();
+    }
+    return instance;
+  }
+}
